38e74d9a6be59e586d5ab59128b7cf50c2218737
Hussain-Safwan <safwan.du16@gmail.com>
2020-01-13 23:50:45 +0600

    Dashboard progress, added a loader and field change without reload

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+const app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Express Session
+app.use(session({
+  secret: 'secret_KEY',
+  saveUninitialized: true,
+  resave: true
+}));
+
+app.get('/', (req, res) => {
+  return res.render('dashboard');
+})
+
+const PORT = process.env.PORT || 3003;
+app.listen(PORT, function () {
+  console.log('Listening to ', PORT);
+});
+
+// AIzaSyD8LUZ2FcwHr00X9lBGFAw9pwrBVkLiJXg google api key
